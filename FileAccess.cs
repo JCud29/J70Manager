d1669d4d5bf2ac30e5d6023b04fd9f11f752328c
JCud29 <james@cudlipp.com>
2023-02-17 14:15:01 +0000

    created a base method to read from a text file

--- FileAccess.cs
+++ FileAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace J70Manager
 {
     internal class FileAccess
     {
+        public FileAccess() { }
+
+        public List<string> ReadTextFile(string URL) {
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(URL).ToList<string>();
+
+            return new List<string>();
+        }
     }
 }
